1bfdd8e733af513d12ce9985cbf541809a406d3f
emlorraine <52936908+emlorraine@users.noreply.github.com>
2023-12-05 13:55:38 -0800

    chore: adding in react project

---
+++ portfolio/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Profile from './components/profile/Profile';
+
+const App = () => {
+  return (
+    <div>
+      <Profile />
+    </div>
+  );
+};
+
+export default App;
---
+++ portfolio/src/components/profile/Profile.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './Profile.css';
+
+const Profile = () => {
+  return (
+    <div className="profile-container">
+      <div className="profile">
+        <header className="profile-header">
+          <h1>Emma Baker</h1>
+          <h4>👋 I'm a hybrid engineer-journalist.</h4>
+        </header>
+        <section>
+          <p>
+            I build tools that enhance a newsroom's storytelling capacity, deliver forward-thinking solutions that expand the reporting capabilities of journalists, and contribute to high-impact investigative and visual editorial projects.
+          </p>
+          <p>
+            I'm interested in data-driven news projects, visual storytelling and building tools for reporters.
+          </p>
+          <p>
+          💌 Reach out to learn more about my background and chat <a href="https://www.linkedin.com/in/emma-baker-aa5355182/">here</a>.
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
